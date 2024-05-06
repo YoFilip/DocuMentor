e1ea95eb77be1a05ff3720babdd492c1f01056b5
vbmcz <151908083+vbmcz@users.noreply.github.com>
2024-05-06 17:37:53 +0200

    Update script.js
    Migrated code from YoFilip branch to vbmcz branch

--- script.js
+++ script.js
@@ -1,112 +1,131 @@
-document
-	.querySelector(".submit-input")
-	.addEventListener("click", generateComments);
+document.querySelector(".submit-input").addEventListener("click", generateComments);
+//Potrzebujemy:
+//Klasy i konstruktory klas
+//Funckji
+//Zmienne ✔
 
-// document
-// 	.querySelector(".submit-input")
-// 	.addEventListener("click", helper);
+document.getElementById("sourceCode").value = "public static void nawa(int zmiennna){}";
 
-// Funkcja do generowania komentarzy
+var list = {
+  int: [],
+  double: [],
+  float: [],
+  byte: [],
+  short: [],
+  long: [],
+  String: [],
+  boolean: [],
+  char: [],
+};
+
+// Główna funkcja generująca komentarze
 function generateComments() {
-	const code = document.getElementById("sourceCode").value;
+	list = {
+	int: [],
+	double: [],
+	float: [],
+	byte: [],
+	short: [],
+	long: [],
+	String: [],
+	boolean: [],
+	char: [],
+};
+const code = document.getElementById("sourceCode").value;
 	const lines = code.split(/;\s*|\n/);
-	let result = "";
-
-	for (let line of lines) {
-		const trimmedLine = line.trim();
-		if (trimmedLine) {
-			if(trimmedLine[trimmedLine.length - 1] === "{"){
-				var note = generateCommentForClass(trimmedLine);
-			}else{
-				var note = generateCommentForLine(trimmedLine + ";");
-			}
-			if (note) {
-				if (result) {
-					result += "\n";
-				}
-				result += note;
-			}
-		}
+	let result = lines.map((line) => {
+	const trimmedLine = line.trim();
+	if (trimmedLine) {
+		const note = generateCommentForLine(trimmedLine + ";");
+		return note ? note : "";
 	}
+	return "";
+	}).join("\n");
 	document.getElementById("output").innerHTML = result;
 }
 
-// function generateComments() {
-// 	const code = document.getElementById("sourceCode").value;
-// 	const lines = code.split(/;\s*|\n/);
-// 	let result = "";
+// Funkcja do generowania komentarzy dla funkcji
+function generateCommentForFuncs(line){
+	let pattern1 = /^(public|private|protected)?\s*(static | abstract)?\s*(int|String|double|float|boolean|char|byte|short|long|void)\s+([a-zA-Z_][a-zA-Z0-9_]*)\((.*?)\)\s*.*({|;|})$/;
+		if (pattern1.test(line)) {
+			let arr = line.match(pattern1);
+			let [, visibility, modifier, returnType, functionName, params] = arr;
+			if (params) {
+				let paramsArray = params.split(',').map(param => param.trim().split(' '));
+				arr.length - 1;
+				var paramsComment = paramsArray.map(param => ` * @param ${param[0]} ${param[1]}`).join('\n');
+			}
+		return `/**\n * Zdefiniowano ${visibility ? visibility : ''} ${modifier ? modifier : ''} funkcję ${returnType} o nazwie ${functionName}\n${paramsComment}\n */`;
+	}else{
+		return null;
+	}
+}
 
-// 	for (let line of lines) {
-// 		const trimmedLine = line.trim();
-// 		if (trimmedLine) {
-// 			let note = generateCommentForClass(trimmedLine);
-// 			if (!note) {
-//                 note = generateCommentForLine(trimmedLine + ";");
-//             }
-// 			if (note) {
-// 				if (result) {
-// 					result += "\n";
-// 				}
-// 				result += note;
-// 			}
-// 		}
-// 	}
-// 	document.getElementById("output").innerHTML = result;
-// }
-
-
-// Funkcja do parsowania zmiennych
-function generateCommentForLine(line) {
-	const pattern =
-		/^(int|String|double|float|boolean|char|byte|short|long)\s+([a-z_][a-z0-9_]*)\s*(=\s*(.*))?;$/;
-	const match = line.match(pattern);
+// Funkcja do generowania komentarzy dla zmiennych (razem z zmiennymi tablicowymi)
+function generateCommentsForVariables(line){
+	const pattern = /^(int|String|double|float|boolean|char|byte|short|long)(\[\])?\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(=\s*({[^}]*}|.*));$/;
+ 	const match = line.match(pattern);
 
 	if (match) {
-		const type = match[1];
-		const name = match[2];
-		const value = match[4];
-		if (value === undefined) {
-			return `/**\n * Zdefiniowano zmienną ${type} ${name}\n */`;
-		} else {
-			const isValid = validateVariable(type, value);
-			if (isValid) {
-				return `/**\n * Zdefiniowano zmienną ${type} ${name} o wartości ${value}\n */`;
+		const [, type, isArray, name, , value] = match;
+
+		if (isArray) {
+			if (value) {
+				return `/**\n * Zdefiniowano tablicę ${type} ${name} z wartościami ${value}\n */`;
 			} else {
-				return `<span class="error">ERROR: Niepoprawna definicja zmiennej: ${name}</span>`;
+				return `/**\n * Zdefiniowano pustą tablicę ${type} ${name}\n */`;
+			}
+		} else {
+			if (value === undefined) {
+				return `/**\n * Zdefiniowano zmienną ${type} ${name}\n */`;
+			} else {
+				if (validateVariable(type, value)) {
+					if (list[type].includes(name)) {
+						return `<span class="error">ERROR: Zmienna ${name} została już zainicjowana</span>`;
+					}
+					list[type].push(name);
+					return `/**\n * Zdefiniowano zmienną ${type} ${name} o wartości ${value}\n */`;
+				} else {
+					return `<span class="error">ERROR: Niepoprawna definicja zmiennej: ${name}</span>`;
+				}
 			}
 		}
+	}else{
+		return null;
 	}
-	return null;
 }
 
-// Funkcja do parsowania klas
-function generateCommentForClass(line) {
-    const pattern =
-        /^(class | public |abstract|final)\bclass\b\s+([A-Za-z][A-Za-z0-9])/;
-    const match = line.match(pattern);
-    if (match) {
-        const modifiers = match[1] ? match[1].trim() : "class";
-        const name = match[2];
-        return `/**\n * ${modifiers} klasa o nazwie ${name}\n */\n${line}`;
-    }
-    return null;
+// Funkcja zwracająca wygenerowane komentarze
+function generateCommentForLine(line) {
+	let checker;
+	if(checker = generateCommentForFuncs(line)){
+		return checker;
+	}else if(checker = generateCommentsForVariables(line)){
+		return checker;
+	}else{
+		return null;
+	}
 }
 
-// Funkcja do sprawdzania zmiennych
+// Funkcja do sprawdzania poprawności zmiennych
 function validateVariable(type, value) {
-	if (["int", "double", "float", "byte", "short", "long"].includes(type)) {
-		return (
-			/^-?\d+(\.\d+)?$/.test(value) &&
-			!value.startsWith('"') &&
-			!value.endsWith('"')
-		);
-	} else if (type === "String") {
-		return value.startsWith('"') && value.endsWith('"');
-	} else if (type === "boolean") {
-		return value === "true" || value === "false";
-	} else if (type === "char") {
-		return value.startsWith("'") && value.endsWith("'") && value.length === 3;
-	} else {
-		return false;
-	}
+  if (["int", "double", "float", "byte", "short", "long"].includes(type)) {
+    return /^-?\d+(\.\d+)?$/.test(value) && !value.startsWith('"') && !value.endsWith('"');
+  } else if (type === "String") {
+    if (value.startsWith('"') && value.endsWith('"')) {
+      return true;
+    } else if (value.startsWith('["') && value.endsWith('"]')) {
+      return true;
+    }
+    return false;
+  } else if (type === "boolean") {
+    return value === "true" || value === "false";
+  } else if (type === "char") {
+    return value.startsWith("'") && value.endsWith("'") && value.length === 3;
+  } else {
+    return false;
+  }
 }
+
+
+
