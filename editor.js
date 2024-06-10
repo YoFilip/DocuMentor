519d2c14cdfe8e56db748e01aff33fd1c9bf3b1b
Yo FIlip <filipxpp@wp.pl>
2024-06-10 18:40:15 +0200

    feat: Fixed generating comments for functions (TODO: Generating comments for functions conflicts with comments for constructors)

--- editor.js
+++ editor.js
@@ -29,8 +29,7 @@ resetButton.addEventListener("click", function () {
 function addAuthorAndVersion() {
 	let author = authorInput.value;
 	let version = versionInput.value;
-    if(author === "" || version === "")
-        return;
+	if (author === "" || version === "") return;
 	let code = aceEditor.getValue();
 	let comment = `/**\n * @author: ${author}\n * @version: ${version}\n*/\n`;
 	aceEditor.setValue(comment + code);
@@ -66,24 +65,26 @@ generateButton.addEventListener("click", function () {
 		if (classComment) {
 			comments += classComment + "\n";
 		}
-        let variableComment = generateCommentForVars(block, types);
-        if (variableComment) {
-            comments += variableComment;
-        }
+		let variableComment = generateCommentForVars(block, types);
+		if (variableComment) {
+			comments += variableComment;
+		}
 		if (classNames.length !== 0) {
-            let constructorComment = generateCommentForConstructors(block, classNames);
-            if(constructorComment)
-                comments += constructorComment;
-        }
-        let functionComment = generateCommentForFuncs(block);
-        if (functionComment) {
-            comments += functionComment;
-        }
+			let constructorComment = generateCommentForConstructors(
+				block,
+				classNames
+			);
+			if (constructorComment) comments += constructorComment;
+		}
+		let functionComment = generateCommentForFuncs(block);
+		if (functionComment) {
+			comments += functionComment;
+		}
 	});
 
 	aceEditor.setValue(comments);
 	addAuthorAndVersion();
-    aceEditor.setValue(aceEditor.getValue() + '\n}')
+	aceEditor.setValue(aceEditor.getValue() + "\n");
 });
 var functionPattern =
 	/^(public|private|protected)?\s*(static|abstract)?\s*(\w+)\s+(\w+)\((.*?)\)\s*\{([^}]*)\}/g;
@@ -194,26 +195,32 @@ function generateCommentForClass(block) {
 		let className = classMatch[2];
 		let fields = classMatch[3].split("\n");
 		let body = "";
-        let fieldCheckerArr = [];
-        let fieldIter = 0;
-        fields.forEach(elm=>{
-            ++fieldIter
-            if(/[a-zA-Z]/.test(elm) && elm !== "" && elm !== '\r'){
-                fieldCheckerArr[fieldIter] = true;
-            }
-        });
+		let fieldCheckerArr = [];
+		let fieldIter = 0;
+		fields.forEach((elm) => {
+			++fieldIter;
+			if (/[a-zA-Z]/.test(elm) && elm !== "" && elm !== "\r") {
+				fieldCheckerArr[fieldIter] = true;
+			}
+		});
 
-        let iter = 0;
-        for(var i = 0; i < fieldCheckerArr.length; ++i){
-            if(fieldCheckerArr[i]){
-                while(fields[iter].charAt(fields[iter].length - 2) == ";" || fields[iter].trim() === "") {
-                    if (!fields[iter].includes("abstract") && fields[iter].trim() !== "") {
-                        body += fields[iter] + "\n";
-                    }
-                    ++iter;
-                }
-            }
-        }
+		let iter = 0;
+		for (var i = 0; i < fieldCheckerArr.length; ++i) {
+			if (fieldCheckerArr[i]) {
+				while (
+					fields[iter].charAt(fields[iter].length - 2) == ";" ||
+					fields[iter].trim() === ""
+				) {
+					if (
+						!fields[iter].includes("abstract") &&
+						fields[iter].trim() !== ""
+					) {
+						body += fields[iter] + "\n";
+					}
+					++iter;
+				}
+			}
+		}
 
 		return `/**\n * Zdefiniowano ${
 			getModifierF(classMod) !== "" ? getModifierF(classMod) + " " : ""
